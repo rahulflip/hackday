2d7033afc20c1df2c207d0fd00550fc3247ea209
Manas Chhabra <manas.chhabra@PW-C02VW1ZJG8WN.local>
2018-06-15 02:03:33 +0530

    dto added

---
+++ src/main/java/com/dto/DeliveryBoyRequest.java
@@ -0,0 +1,19 @@
+package com.dto;
+
+public class DeliveryBoyRequest {
+
+	private Integer deliveryHub;
+	private Integer orderCount;
+	public Integer getDeliveryHub() {
+		return deliveryHub;
+	}
+	public void setDeliveryHub(Integer deliveryHub) {
+		this.deliveryHub = deliveryHub;
+	}
+	public Integer getOrderCount() {
+		return orderCount;
+	}
+	public void setOrderCount(Integer orderCount) {
+		this.orderCount = orderCount;
+	}
+}
